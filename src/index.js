e6f57a5d8ddaab320c50ec0ed881f493ba9923af
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2023-01-31 18:38:53 -0300

    feat: connection database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const pool = require('./Database/DBConnection')
 
 const app = express()
 
@@ -7,6 +8,15 @@ app.use(express.urlencoded({extended:false}))
 
 app.get('/', async (req, res) => {
     res.status(200).send('App iniciado')
+})
+
+app.get('/getusers', async (req, res) => {
+    try {
+        const {rows} = await pool.query('SELECT * FROM users')
+        return res.status(200).send(rows)
+    } catch (err) {
+        return res.status(400).send(err)
+    }
 })
 
 app.listen(3333)
