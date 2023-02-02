7b27c5a32618c195498f8e9c382eb2cece2147bc
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2023-02-02 11:32:46 -0300

    feat: add login and token authentication

--- src/Database/DBConnection.js
+++ src/Database/DBConnection.js
@@ -1,19 +1,19 @@
 const pg = require('pg');
 // String de conexão com BD
-let connString = "postgres://sangnsjj:7mX6_XfRpAVr_j_PhoC6F_so_ZnHQ3ni@babar.db.elephantsql.com/sangnsjj"
+let connString = "postgres://sangnsjj:7mX6_XfRpAVr_j_PhoC6F_so_ZnHQ3ni@babar.db.elephantsql.com/sangnsjj";
 // Abre uma porta para se comunicar com o BD
-let client = new pg.Client(connString)
+let client = new pg.Client(connString);
 // Permite que esta API faça queries (consultas) no BD
 client.connect(function(err) {
     if(err) {
-        return console.error('could not connect to postgres', err)
+        return console.error('could not connect to postgres', err);
     }
     client.query('SELECT NOW() AS "theTime"', function(err, result) {
         if(err) {
             return console.error('error running query', err);
         }
-        console.log(result.rows[0].theTime)
+        console.log(result.rows[0].theTime);
     })
 })
 
-module.exports = client
+module.exports = client;
